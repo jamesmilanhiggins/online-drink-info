ec48923e95a20e77817cfaeaa49e6bac027952f1
Jason Ainsworth and Marko Ludlow <jason@>
2017-04-10 14:24:01 -0700

    add in random number generator, add in recipebook object, add in values for recipe objects, and click function that generates a random number that assocites with the value property of the recipe object

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+var randomNumberGenerator = function() {
+  return Math.floor(Math.random() *3 + 1)
+};
+function RecipeBook (recipes) {
+  this.recipes = [];
+};
+
 //-------------- Constructor for recipe object
 function Recipe(name,ingredients,instructions, value) {
   this.name = name;
@@ -9,10 +16,15 @@ function Recipe(name,ingredients,instructions, value) {
 var mule = new Recipe ("Moscow Mule", "instructions");
 mule.ingredients.push("1 1/2 oz. Vodka", "1/2 oz. fresh lime juice", "1/4oz simple syrup", "ginger beer", "lime wedges for garnish");
 mule.instructions = "Lightly shake vodka, lime juice, and simple syrup. Add ginger beer and shake. Serve in a copper mug and garnish with lime wedge."
+mule.value = 1;
 
 var bloodyMary = new Recipe ("Bloody Mary" , "instructions")
 bloodyMary.ingredients.push("4 oz. Sacramento tomato juice" , "2 dashes Worcestershire sauce" , "pinch of celery salt", "Pinch of freshly ground black pepper", "3 dashes Tabasco sauce" , "1 squeeze lemon" , "1 tsp. fresh horseradish" , "2 oz. vodka, preferably Finlandia" , "Olives, for garnish" , "Lemon wedge, for garnish")
 bloodyMary.instructions = "Make Spicy Bloody Mary Mix: Combine tomato juice, Worcestershire, celery salt, black pepper, Tabasco, lemon, and horseradish. Vigorously shake vodka and bloody mary mix. Garnish with olives and lemon wedge."
+bloodyMary.value = 2;
+
+var newRecipeBook = new RecipeBook ();
+newRecipeBook.recipes.push(mule, bloodyMary);
 
 //---------method to list each ingredient within the recipe
 Recipe.prototype.displayIngredients = function () {
@@ -23,6 +35,20 @@ Recipe.prototype.displayIngredients = function () {
   });
 };
 
-function displayToPage() {
+$(document).ready(function(){
 
-};
+  $("#random-number-button").click(function(){
+    var randomNumber = randomNumberGenerator();
+
+
+
+    newRecipeBook.recipes.forEach(function(recipe){
+      var recipeValue = recipe.value;
+    if (randomNumber === recipeValue) {
+      $(".display").text(recipe.name);
+    } else {
+         alert("3");
+       };
+    });
+  });
+});
